9d0b9ae77a949709ca436980d687a2c9d0b0064a
Exoutia <jhav365@gmail.com>
2023-06-20 11:30:53 +0530

    creating the model for the story with chapters, categories, and comments

--- story/models.py
+++ story/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
+class Stroy(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    catergories = models.ManyToManyField("Category", related_name="stories")
+    taggit = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    story = models.ForeignKey(Stroy, on_delete=models.CASCADE, related_name="chapters")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    story = models.ForeignKey(Stroy, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content

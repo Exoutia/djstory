a641ff1e0a35ef684b178f7b4c693b00d99e8b85
Exoutia <jhav365@gmail.com>
2023-06-20 14:12:27 +0530

    adding the main story page

--- story/views.py
+++ story/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def story(request):
+    story = models.Story.objects.all()
+    return render(request, 'story/index.html', {'story': story})

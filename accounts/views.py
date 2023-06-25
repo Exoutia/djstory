c637f37012bf7581969db9ac8c905e1a3179a9ee
Exoutia <jhav365@gmail.com>
2023-06-25 13:08:12 +0530

    editing the navbar

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("home")
+    return HttpResponse("accounts-home")
 
 def login(request):
     return HttpResponse("login")

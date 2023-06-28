c4b37f6d06db248d7603ea18bb83cc0dc49306af
Exoutia <jhav365@gmail.com>
2023-06-28 09:57:02 +0530

    create login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,15 @@
 from django import forms 
 
-class ContanteForm(forms.ModelForm):
-    pass
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=50, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, label="Password", max_length=50, required=True)
+
+class RegisterForm(forms.Form):
+    firstname = forms.CharField(label="First Name", max_length=50, required=True)
+    lastname = forms.CharField(label="Last Name", max_length=50, required=True)
+    username = forms.CharField(label="Username", max_length=50, required=True)
+    email = forms.EmailField(label="Email", required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password", max_length=50, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password", max_length=50, required=True)
+    
+        
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -7,7 +9,21 @@ def home(request):
     return render(request, 'accounts/index.html')
 
 def login(request):
-    return HttpResponse("login")
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(request, 'accounts/index.html')
+            else:
+                return render(request, 'login.hmtl', {'form': form, 'messages': ['Username or password is wrong']})
+        else:
+            return render(request, 'login.hmtl', {'form': form})
+            
+        
 
 def register(request):
     return HttpResponse("register") 
